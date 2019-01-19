d7f473590a6d78edb43e51ecb0441b7583c07944
Hygde <samuel.devals@outlook.fr>
2019-01-19 19:24:01 +0100

    SendJson uses its own sendData function

--- src/server_side/sendjson.py
+++ src/server_side/sendjson.py
@@ -1,5 +1,6 @@
 import json
-from client import *
+import socket
+from client import Client
 
 class SendJson(Client):
 
@@ -7,12 +8,20 @@ class SendJson(Client):
         Client.__init__(self, TCP, host, port)
         self.path = path
 
+    def sendData(self,data):
+        self.logger.debug("sending data to the server")
+        self.logger.debug(data)
+        if(self.isTCP):self.sock.send(data)
+        else:self.sock.sendto(data, self.host)
+
     def sendingJson(self):
         self.connect()
-        self.sendJsonData(self.getJson())
+        self.sendData(self.getJson())
 
     def setJson(self, path):
         self.path = path
 
     def getJson(self):
-        return json.load(open(self.path))
+        data = json.load(open(self.path))
+        data["services"][0]["ip_local"] = socket.gethostbyname(socket.gethostname())
+        return json.dumps(data).encode()
