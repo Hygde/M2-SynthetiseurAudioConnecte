e031df934f13c831f6a961e3e6d90a0dc3bbd445
Hygde <samuel.devals@outlook.fr>
2019-01-19 19:22:53 +0100

    The server is now sending json

--- src/server_side/main.py
+++ src/server_side/main.py
@@ -4,10 +4,10 @@ from server import Server
 
 def initLogger():
 	logger = logging.getLogger()
-	logger.setLevel(logging.ERROR)
+	logger.setLevel(logging.DEBUG)
 	formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(module)s :: %(funcName)s :: %(message)s")
 	ch = logging.StreamHandler()
-	ch.setLevel(logging.ERROR)
+	ch.setLevel(logging.DEBUG)
 	ch.setFormatter(formatter)
 	logger.addHandler(ch)
 	return logger		
@@ -16,19 +16,15 @@ def initLogger():
 def main():
 	logger = initLogger()
 	logger.info("starting program")
-	
 	assert (len(sys.argv) == 2),"A port is expected"
-	print(int("5"))
 	try:
 		serv = Server(int(sys.argv[1]))
 		logger.debug("Creation of the server successed")
-		#serv.sendJson()
+		serv.sendJson()
 		serv.createMicrophone()
 		serv.whileAccept()
-	except ValueError as e:
+	except Exception as e:
 		logger.error(e)
-	except OSError as e:
-		logger.debug(e)
 
 
 if __name__ == "__main__":
